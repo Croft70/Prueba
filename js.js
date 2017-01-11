92aba58476416243aa5e2d07e70748d4f20029d8
Croft70 <croft70@gmail.com>
2017-01-11 18:17:02 -0400

    agregar actualizar 2

--- js.js
+++ js.js
@@ -20,6 +20,101 @@ function actualizar(){
     nombres.length = 0;
     peticionHTML(0);
 }
+function actualizar2(){
+    chat.length = 0;
+    nombres.length = 0;
+    
+    var ultimosMensajes = document.getElementsByClassName("last-post-icon");
+    var direccionUltimaPagina = ultimosMensajes[ultimosMensajes.length-1].getAttribute("href");
+    direccionUltimaPagina = direccionUltimaPagina.substr(1);
+    var numeroUltimaPagina = direccionUltimaPagina.substr(4,2);
+    console.log(direccionUltimaPagina);
+    console.log(numeroUltimaPagina);
+    // ultima pagina
+    
+    var peticion1 = new XMLHttpRequest();
+    peticion1.onreadystatechange = function(){
+        if (peticion1.readyState==4){
+            if(peticion1.status==200){
+            // parsear mijo
+                var temp = document.createElement("temp");
+                temp.innerHTML = this.responseText;
+                var lista = temp.getElementsByClassName("entry-content");
+                var listaNombres = temp.getElementsByClassName("username");
+                var listaFechas = temp.getElementsByClassName("posthead");
+                
+                if(!(temp.getElementsByClassName("message").length > 0)){
+                    for (var x = 0; x<lista.length;x++){
+                        if(lista[x].getElementsByTagName("script").length>0) continue;
+                        fechas.push(jQuery(listaFechas[x].innerHTML).clone().children().remove().end().text());
+                        chat.push(jQuery(lista[x].getElementsByTagName("div")[0].innerHTML).text());
+                        var usuario = jQuery(listaNombres[x].innerHTML).text();
+                        if(usuario == "") usuario = "Invitado";
+                        nombres.push(usuario);
+                    }
+                    console.log("Encontre algo status: " + peticion1.status);
+                    // segunda peticion
+                    segundaPeticion(numeroUltimaPagina-25);
+                }
+                else{
+                    console.log("Fin busqueda de mensajes");
+                    crearHTML(chat);
+                }
+            }
+            else{
+                
+                    console.log("Fin busqueda de mensajes");
+                    crearHTML(chat);
+            }
+        }
+    }
+    peticion1.open("GET","t31p"+numeroUltimaPagina+"-tagboard-ignorar",true);
+    peticion1.send();
+    
+    
+}
+
+function segundaPeticion(numero){
+    var peticion1 = new XMLHttpRequest();
+    peticion1.onreadystatechange = function(){
+        if (peticion1.readyState==4){
+            if(peticion1.status==200){
+            // parsear mijo
+                var temp = document.createElement("temp");
+                temp.innerHTML = this.responseText;
+                var lista = temp.getElementsByClassName("entry-content");
+                var listaNombres = temp.getElementsByClassName("username");
+                var listaFechas = temp.getElementsByClassName("posthead");
+                
+                if(!(temp.getElementsByClassName("message").length > 0)){
+                    for (var x = lista.length-1; x>=0;x--){
+                        if(lista[x].getElementsByTagName("script").length>0) continue;
+                        fechas.unshift(jQuery(listaFechas[x].innerHTML).clone().children().remove().end().text());
+                        chat.unshift(jQuery(lista[x].getElementsByTagName("div")[0].innerHTML).text());
+                        var usuario = jQuery(listaNombres[x].innerHTML).text();
+                        if(usuario == "") usuario = "Invitado";
+                        nombres.unshift(usuario);
+                    }
+                    console.log("Encontre algo status: " + peticion1.status);
+                    
+                    crearHTML(chat);
+                }
+                else{
+                    console.log("Fin busqueda de mensajes");
+                    crearHTML(chat);
+                }
+            }
+            else{
+                
+                    console.log("Fin busqueda de mensajes");
+                    crearHTML(chat);
+            }
+        }
+    }
+    peticion1.open("GET","t31p"+numero+"-tagboard-ignorar",true);
+    peticion1.send();
+}
+
 function crearHTML(chat){
     var color1 = "rgba(229, 299, 299, 0.5)";
     var color2 = "rgba(243, 244, 245, 0.5)";
@@ -28,6 +123,7 @@ function crearHTML(chat){
     document.getElementById("a").innerHTML = "";
     jQuery('#a').empty();
     for(var x = 0; x<chat.length;x++){
+        if(x==20) break;
         div.innerHTML += "<div style='padding-top:0px; padding-bottom:0px; background-color : "+ ((x%2 == 0)? color1 : color2) +";'>"+ nombres[x] + ": " + chat[x] + "<div style='line-height: 70%;padding-bottom:0px;font-size:0.8em;width:100%; text-align: right'>Enviado"+ fechas[x]+ "</div>" + "</div>";
     }
     div.scrollTop = div.scrollHeight - div.clientHeight;
