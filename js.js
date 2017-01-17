240396a31328510493149efe52a534667e877957
Croft70 <croft70@gmail.com>
2017-01-17 17:39:14 -0400

    Update js.js

--- js.js
+++ js.js
@@ -47,7 +47,7 @@ function actualizar1(){
     var ultimosMensajesTcr = document.getElementsByClassName("tcr");
     var ultimosMensajes = ultimosMensajesTcr[ultimosMensajesTcr.length-1].getElementsByTagName("span")[0].getElementsByTagName("a")[1];
     //var direccionUltimaPagina = ultimosMensajes[ultimosMensajes.length-1].getAttribute("href");
-    var direccionUltimaPagina = ultimosMensajes[ultimosMensajes.length-1].getAttribute("href");
+    var direccionUltimaPagina = ultimosMensajes.getAttribute("href");
     direccionUltimaPagina = direccionUltimaPagina.substr(1);
     var numeroUltimaPagina = direccionUltimaPagina.split("-")[0].split("p")[1];
     console.log("cantidad de temas: " + ultimosMensajes.length);
